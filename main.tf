0b7dbad8e9389f45282cdc1a6b2dde050a7aefbf
nazreen07 <nazreen_banu@outlook.com>
2024-11-11 23:53:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,14 +65,14 @@ data "aws_ami" "ubuntu_ami" {
   }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "nazreens3bucket"
-}
-
 resource "aws_launch_template" "ubuntu_launch_template" {
   name          = "ubuntu-launch-template"
   image_id      = data.aws_ami.ubuntu_ami.id
   instance_type = "t2.micro"
+
+  monitoring {
+    enabled = true  # Enables detailed monitoring
+  }
 
   network_interfaces {
     security_groups = [aws_security_group.sg.id]
@@ -113,7 +113,7 @@ resource "aws_autoscaling_schedule" "scale_up_am" {
   desired_capacity     = 2
   min_size             = 2
   max_size             = 2
-  start_time           = "2024-11-10T21:45:00Z"
+  recurrence             = "0 5 * * *"  # Every day at 5 AM UTC (6 AM CET)
 }
 
 resource "aws_autoscaling_schedule" "scale_down_pm" {
@@ -122,12 +122,32 @@ resource "aws_autoscaling_schedule" "scale_down_pm" {
   desired_capacity     = 0
   min_size             = 0
   max_size             = 0
-  start_time           = "2024-11-10T21:50:00Z"
+  recurrence             = "0 17 * * *"  # Every day at 5 PM UTC (6 PM CET)
 }
 
-resource "aws_lambda_function" "ec2_termination_report" {
+# SNS Topic for Alarm Notifications
+resource "aws_sns_topic" "ec2_alarm_topic" {
+  name = "ec2-instance-state-change-topic"
+}
+
+# Subscription to SNS Topic for CloudWatch Alarm notifications only
+resource "aws_sns_topic_subscription" "ec2_alarm_email_subscription" {
+  topic_arn = aws_sns_topic.ec2_alarm_topic.arn
+  protocol  = "email"
+  endpoint  = "banunazreen73@gmail.com"  # Directly sends alarm notification to this email
+}
+
+# Lambda Subscription to SNS Topic for handling other events
+resource "aws_sns_topic_subscription" "ec2_alarm_lambda_subscription" {
+  topic_arn = aws_sns_topic.ec2_alarm_topic.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.ec2_uptime_check.arn
+}
+
+# Lambda Function for EC2 State-Change Events (Running/Terminated) and Billing Notification
+resource "aws_lambda_function" "ec2_uptime_check" {
   filename         = "lambda.zip"
-  function_name    = "ec2_termination_report"
+  function_name    = "ec2_uptime_check"
   role             = aws_iam_role.lambda_exec_role.arn
   handler          = "lambda.lambda_handler"
   runtime          = "python3.8"
@@ -140,6 +160,7 @@ resource "aws_lambda_function" "ec2_termination_report" {
   }
 }
 
+# IAM Role for Lambda Execution
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
 
@@ -157,6 +178,7 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
+# Attach Policies for Lambda Permissions
 resource "aws_iam_role_policy_attachment" "lambda_cloudwatch_logs" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
@@ -170,19 +192,48 @@ resource "aws_iam_role_policy" "lambda_policy" {
     Version   = "2012-10-17"
     Statement = [
       {
-        Action    = "ses:SendEmail"
-        Effect    = "Allow"
+        Action    = "ses:SendEmail",
+        Effect    = "Allow",
         Resource  = "*"
       },
       {
-        Action    = "ce:GetCostAndUsage"
-        Effect    = "Allow"
+        Action    = "ce:GetCostAndUsage",
+        Effect    = "Allow",
         Resource  = "*"
+      },
+      {
+        Action    = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeInstanceStatus"
+        ],
+        Effect    = "Allow",
+        Resource  = "*"
+      },
+      {
+        Action    = "sns:Publish",
+        Effect    = "Allow",
+        Resource  = aws_sns_topic.ec2_alarm_topic.arn
       }
     ]
   })
 }
 
+# CloudWatch Alarm for Instance Running Over 12 Hours Based on CPU Utilization
+resource "aws_cloudwatch_metric_alarm" "instance_running_over_12_hours" {
+  alarm_name          = "InstanceRunningOver10Minutes"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 12              # 12 periods of 1 hour each
+  metric_name         = "CPUUtilization" # Using CPU utilization as an indicator of running state
+  namespace           = "AWS/EC2"
+  period              = 3600               # 1 hour intervals
+  statistic           = "Average"
+  threshold           = 1                # Low threshold to detect running state
+
+  alarm_description = "Alarm if EC2 instance is running for more than 10 minutes."
+  alarm_actions     = [aws_sns_topic.ec2_alarm_topic.arn]
+}
+
+# CloudWatch Event Rule to Trigger Lambda on EC2 State Change
 resource "aws_cloudwatch_event_rule" "ec2_state_change_rule" {
   name        = "ec2-state-change-rule"
   description = "Trigger Lambda function when an EC2 instance changes state"
@@ -197,43 +248,13 @@ resource "aws_cloudwatch_event_rule" "ec2_state_change_rule" {
 
 resource "aws_cloudwatch_event_target" "ec2_state_change_target" {
   rule = aws_cloudwatch_event_rule.ec2_state_change_rule.name
-  arn  = aws_lambda_function.ec2_termination_report.arn
+  arn  = aws_lambda_function.ec2_uptime_check.arn
 }
 
 resource "aws_lambda_permission" "allow_eventbridge" {
   statement_id  = "AllowExecutionFromEventBridge"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.ec2_termination_report.function_name
+  function_name = aws_lambda_function.ec2_uptime_check.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.ec2_state_change_rule.arn
-}
-
-# SNS Topic and Subscription for Alarm Notifications
-resource "aws_sns_topic" "ec2_alarm_topic" {
-  name = "ec2-instance-state-change-topic"
-}
-
-resource "aws_sns_topic_subscription" "ec2_alarm_subscription" {
-  topic_arn = aws_sns_topic.ec2_alarm_topic.arn
-  protocol  = "email"
-  endpoint  = "banunazreen73@gmail.com"  # Replace with actual email address
-}
-
-# CloudWatch Alarm to Monitor Instances Overnight
-resource "aws_cloudwatch_metric_alarm" "instance_running_overnight" {
-  alarm_name          = "InstanceRunningOvernightAlarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = 3600            # Check every hour
-  statistic           = "Average"
-  threshold           = 1               # Triggers if usage is above 1%
-
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.ubuntu_asg.name
-  }
-
-  alarm_description = "Alarm if EC2 instance is running overnight."
-  alarm_actions     = [aws_sns_topic.ec2_alarm_topic.arn]
 }
